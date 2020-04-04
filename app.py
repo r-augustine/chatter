f54955fde01fd9bb6dabae645b8d2914beb997d4
Ricardo Augustine <augustineri93@gmail.com>
2020-04-03 21:19:06 -0400

    added sqlite3 db and user model

--- app.py
+++ app.py
@@ -1,8 +1,21 @@
 from flask import Flask, render_template, url_for
 from wtform_fields import *
+#from models import *
+
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
 app.secret_key = 'replace later!'
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(), nullable=False)
+
 
 @app.route('/', methods=['GET'])
 def index():
@@ -13,7 +26,9 @@ def login():
 
     login_form = LoginForm()
     if login_form.validate_on_submit():
-        return 'you tried to login'
+        username = login_form.username.data
+        password = login_form.password.data
+        return 'Great success'
 
     return render_template('login.html', form=login_form)
 
