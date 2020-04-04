310c32797a916f2f3a2777aff79a04fb06c26ecd
Ricardo Augustine <augustineri93@gmail.com>
2020-04-03 20:29:35 -0400

    added validation to login form using flask-wtf

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 from wtform_fields import *
 
 app = Flask(__name__)
@@ -12,6 +12,8 @@ def index():
 def login():
 
     login_form = LoginForm()
+    if login_form.validate_on_submit():
+        return 'you tried to login'
 
     return render_template('login.html', form=login_form)
 
--- wtform_fields.py
+++ wtform_fields.py
@@ -1,9 +1,18 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField
+from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import InputRequired, Length, EqualTo
 
 class LoginForm(FlaskForm):
     '''Login Form'''
 
-    username = StringField('username')
-    password = PasswordField('password_label')
+    username = StringField('username', 
+        validators=[InputRequired(
+            message='Username required'),
+            Length(min=4, max=25, message='Username must be between 4 and 25 characters')
+        ])
+    password = PasswordField('password_label',
+        validators=[InputRequired(
+            message='Username required'),
+            Length(min=4, max=25, message='Username must be between 4 and 25 characters')
+        ])
+    submit_button = SubmitField('Sign In')
